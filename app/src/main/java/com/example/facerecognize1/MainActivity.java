64df36c010829558fe68d3ffdc865fc957170342
Your Name <you@example.com>
2020-10-02 15:59:56 +0700

    1

---
+++ app/src/main/java/com/example/facerecognize1/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.facerecognize1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("Marvel");
+        }
+
+        Button recognizeButton = (Button) findViewById(R.id.recognizeButton);
+        Button trainingButton = (Button) findViewById(R.id.trainingButton);
+
+        recognizeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Recognize.class));
+            }
+        });
+
+        trainingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, NameActivity.class));
+            }
+        });
+
+    }
+}
